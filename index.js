c0a8ad44508baea5c6d4e3e06270068e2f2b7a75
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-03-29 09:22:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var katzDeli = [];
 
-function takeANumber(name) {
-  katzDeli.unshift(name)
-  return katzDeli
+function takeANumber(katzDeliLine, name) {
+  katzDeli.push(name)
+  for (var katzDeliLine = 0; katzDeliLine < katzDeli.length; katzDeliLine++) {
+    return `Welcome, ${name}. You are number ${katzDeliLine} in line.`
+  }
 }
