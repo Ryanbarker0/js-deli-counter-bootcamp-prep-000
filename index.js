a7d4050d3e88522784bc7f84927ea28ec9880098
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-03-29 10:39:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nowServing(deliLine) {
   if (katzDeliLine.length === 0) {
     return 'There is nobody waiting to be served!'
   } else {
-    return `Now serving ${deliLine}`
+    return `Now serving ${deliLine[0]}`
       katzDeliLine.shift([0])
   }
 }
