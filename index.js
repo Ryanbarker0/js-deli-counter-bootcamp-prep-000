749a5e56338b7df7d29f5ad52f36a12325a53a04
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-03-29 10:39:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ function takeANumber(katzDeliLine, name) {
    return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
-function nowServing() {
+function nowServing(deliLine) {
   if (katzDeliLine.length === 0) {
     return 'There is nobody waiting to be served!'
   } else {
-    return `Now serving ${katzDeliLine[0]}`
+    return `Now serving ${deliLine}`
       katzDeliLine.shift([0])
   }
 }
